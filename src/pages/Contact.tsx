d12779aba05e734f83449151c5d03b2a3de7f61c
andrewhought <80929614+boolin9@users.noreply.github.com>
2025-02-15 11:31:22 -0700

    minor styling change

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,7 +9,7 @@ export default function Contact(): ReactElement {
             id="contact"
             className="mx-12 mt-12 flex flex-col justify-between py-24 md:mx-24 lg:mx-48 xl:mx-72"
         >
-            <span className="flex text-2xl font-extrabold text-header sm:text-3xl md:text-4xl">
+            <span className="mb-8 flex text-2xl font-extrabold text-header sm:text-3xl md:text-4xl">
                 Contact
             </span>
             <ContactForm />
